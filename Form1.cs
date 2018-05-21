46b126153053c3623b46586ed5259ebae940ad72
radhika052 <39478308+radhika052@users.noreply.github.com>
2018-05-21 13:20:45 +0530

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lblMessage.Text = GetMessage(textBox1.Text);
+        }
+
+        public string GetMessage(string name)
+        {
+            var msg= "Hi " + name + "! How are you doing today?";
+            return msg;
+        }
+
+        public string GetMessageWithFullName(string title, string firstname, string lastname)
+        {
+            var msg = "Hi " + title + " " + firstname + " " + lastname + "! How are you doing today?";
+            return msg;
+        }
+    }
+}
